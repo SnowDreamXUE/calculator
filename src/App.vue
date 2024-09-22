abef23312aded570800f63edd399dec87218a887
SnowDreamXUE <1505185702@qq.com>
2024-09-22 17:23:07 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from './components/calculator'
+
+export default {
+  name: 'app',
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<style>
+  body {
+    background: linear-gradient(to right, #85D8CE, #085078); 
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+</style>
